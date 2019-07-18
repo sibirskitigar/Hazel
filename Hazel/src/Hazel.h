c1eaf82ecbfa4fdedc983c26b137695df27efd64
sibirskitigar <44558387+sibirskitigar@users.noreply.github.com>
2019-07-19 07:29:15 +1000

    entry point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//for use by Hazel appliocations
+
+#include <stdio.h>
+#include "Hazel/Application.h"
+
+// -------entry point----------------
+#include "Hazel/EntryPoint.h"
+//-----------------------------------
